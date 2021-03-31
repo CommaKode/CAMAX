9c291a134b0e90dc4a8d7570ab8bdf6fcf545ec5
mehran moalemi <mohammadmoallemi78@gmail.com>
2021-03-31 11:53:37 -0700

    edit

--- js/shopCardGenerator.js
+++ js/shopCardGenerator.js
@@ -12,10 +12,28 @@ shopCardGenerator = () => {
   addBtnSvg();
 };
 shopCardGenerator();
-// console.log(pNameList[0])  
 
 
     
+// document.getElementById("inConnectorOn").onchange = function () {
+//   for (let index = 1; index <= numberOfCards; index++) {
+//     if (this.checked) {
+//         let counter = 0;
+//       let pn1 = document.getElementById(`productName${index}`).innerText;
+      
+//       if (
+//         (pn1.toUpperCase() ===
+//           pNameList[counter] && inConnectorList[counter] == "on")
+//           ) {
+//             document.getElementById(`card${index}`).classList.add("dn");  
+//           }
+//           counter++
+//         }else{
+          
+//           document.getElementById(`card${index}`).classList.remove("dn");  
+//         }
+//         }
+//     };
 document.getElementById("inConnectorOn").onchange = function () {
   for (let index = 1; index <= numberOfCards; index++) {
     if (this.checked) {
